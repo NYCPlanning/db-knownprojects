df848e9f1e41c23e1b17f211ce0a2a52cc695f3a
mgraber <molly.graber@me.com>
2020-04-06 14:38:56 +0000

    Load dcp_planneradded and create _proj table

---
+++ knownprojects_build/sql/dcp_planneradded.sql
@@ -0,0 +1,63 @@
+/****************** Assign bbl geometries ****************/
+ALTER TABLE dcp_planneradded
+RENAME wkb_geometry TO geom;
+
+ALTER TABLE dcp_planneradded
+    ADD project_name text,
+    ADD project_status text,
+    ADD project_type text,
+    ADD number_of_units text,
+    ADD date text,
+    ADD date_type text,
+    ADD dcp_projectcompleted text,
+    ADD date_filed text,
+    ADD date_permittd text,
+    ADD date_lastupdt text,
+    ADD date_complete text,
+    ADD portion_built_by_2025 text,
+    ADD portion_built_by_2035 text,
+    ADD portion_built_by_2055 text,
+    ADD inactive text
+    ;
+
+/********************* Column Mapping *******************/
+UPDATE dcp_planneradded t
+SET project_name = project_na,
+    project_status = NULL,
+    project_type = NULL,
+    number_of_units = total_unit,
+    date = NULL,
+    date_type = NULL,
+    dcp_projectcompleted = NULL,
+    date_filed = NULL,
+    date_permittd = NULL,
+    date_lastupdt = NULL,
+    date_complete = NULL,
+    portion_built_by_2025 = portion_bu,
+    portion_built_by_2035 = portion__1,
+    portion_built_by_2055 = portion__2,
+    inactive = NULL
+    ;
+
+/************************ Merging ***********************/
+-- merge the records to project's level
+DROP TABLE IF EXISTS dcp_planneradded_proj;
+CREATE TABLE dcp_planneradded_proj AS(
+	WITH geom_merge AS (
+		SELECT project_id, ST_UNION(geom) AS geom
+		FROM dcp_planneradded
+		GROUP BY project_id
+	)
+    SELECT b.source, b.project_id, b.project_name,
+    b.project_status, b.project_type, b.inactive,
+    b.number_of_units, b.date, b.date_type, b.dcp_projectcompleted,
+    b.date_filed, b.date_permittd, b.date_lastupdt, b.date_complete,
+    b.portion_built_by_2025,
+    b.portion_built_by_2035, b.portion_built_by_2055,
+    a.geom
+	FROM geom_merge a
+	LEFT JOIN(
+		SELECT DISTINCT ON (project_id) *
+		FROM dcp_planneradded) AS b
+	ON a.project_id = b.project_id
+);
