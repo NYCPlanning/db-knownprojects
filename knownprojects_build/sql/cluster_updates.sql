e563470e1332b5bb1d8b7fb307b1cffbb625850f
mgraber <molly.graber@me.com>
2020-04-06 14:23:07 +0000

    Modify versioning

--- knownprojects_build/sql/cluster_updates.sql
+++ knownprojects_build/sql/cluster_updates.sql
@@ -1,7 +1,7 @@
 -- Update cluster table with manual edits
-UPDATE clusters
+UPDATE clusters."2019" a
 SET sub_cluster_id = b.sub_cluster_id
-FROM reviewed_clusters b
-WHERE source = b.source
-AND project_id = b.project_id
-AND project_name = b.project_name;
+FROM reviewed_clusters."2019" b
+WHERE a.source = b.source
+AND a.project_id = b.project_id
+AND a.project_name = b.project_name;
