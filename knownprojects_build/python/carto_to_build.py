9d3b74007677f874db4c5e0100ce0a37d2c70a5e
mgraber <molly.graber@me.com>
2020-04-06 14:22:49 +0000

    Change versioning and use $ sep

--- knownprojects_build/python/carto_to_build.py
+++ knownprojects_build/python/carto_to_build.py
@@ -1,6 +1,5 @@
 from helper.engines import recipe_engine, edm_engine, build_engine
 from helper.exporter import exporter
-#from resolve_clusters import resolve_all_clusters
 from sqlalchemy import create_engine
 import pandas as pd
 import numpy as np
@@ -21,19 +20,12 @@ print("Loading cluster data from carto...")
 cluster_gdf = read_carto('clusters', limit=100)
 reviewed_gdf = read_carto('clusters_unresolved', limit=100)
 
-'''
-# Resolve clusters
-print("Resolving clusters...")
-reviewed_gdf['adjusted_units'] = reviewed_gdf['number_of_units']
-reviewed_gdf = resolve_all_clusters(reviewed_gdf).drop(columns=['cartodb_id','the_geom'])
-'''
-
 # Export to postgres
 today = date.today()
 date = today.strftime("%Y-%m-%d")
 
-cluster_table = "clusters.\"" + date + "\""
-reviewed_table = "reviewed_clusters.\"" + date + "\""
+cluster_table = "clusters.\"2019\""
+reviewed_table = "reviewed_clusters.\"2019\""
 
 DDL = {"source":"text",
     "project_id":"text",
@@ -50,12 +42,17 @@ DDL = {"source":"text",
     "sub_cluster_id":"text",
     "geom":"geometry(MultiPolygon,4326)"}
 
+
 # Export to build engine
 print("Exporting to build engine...")
 exporter(cluster_gdf, cluster_table, DDL, 
-            con=build_engine, sql='', sep=',', 
+            con=build_engine, 
+            sql='', 
+            sep='$', 
             geo_column='geom', SRID=4326)
 
 exporter(reviewed_gdf, reviewed_table, DDL, 
-            con=build_engine, sql='', sep=',', 
+            con=build_engine,
+            sql='', 
+            sep='$', 
             geo_column='geom', SRID=4326)
