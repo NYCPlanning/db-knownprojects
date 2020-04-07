7e004e123ca48ebfe0dd2a5bcf9b28debcc4c760
mgraber <molly.graber@me.com>
2020-04-07 15:19:16 +0000

    Add update_proj_tables.py to 03.2_housing

--- knownprojects_build/03.2_housing.sh
+++ knownprojects_build/03.2_housing.sh
@@ -11,6 +11,7 @@ docker run --rm\
             -e CARTO_APIKEY=$CARTO_APIKEY\
             python:3.7-slim sh -c "
                 pip3 install -r python/requirements.txt; 
-                python3 python/carto_to_build.py"
+                python3 python/carto_to_build.py;
+                python3 python/update_proj_tables.py"
 
 psql $BUILD_ENGINE -f sql/cluster_updates.sql
