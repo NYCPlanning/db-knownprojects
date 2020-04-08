785f084ef39ca8af3bddfd92692cb3b150f954af
sptkl <caobaiyue@gmail.com>
2020-04-08 13:29:27 +0000

    adding dob matching review table script
    Former-commit-id: 0ddfe5dd1da06d50d206ca1d96482d211e002644

--- knownprojects_build/python/dob_match.py
+++ knownprojects_build/python/dob_match.py
@@ -32,7 +32,41 @@ for i in tables:
 
 dff = pd.concat(r)
 dff=dff.drop_duplicates()
-# g = dff.groupby('project_id')
-# review = g.filter(lambda x: len(x) > 1)
-# review.to_sql('dob_review', build_engine)
 dff.to_sql('dob_match', build_engine)
+
+reviewed_clusters='reviewed_clusters."2020-04-06"'
+df_dob=pd.read_sql(f'''
+    with multimatch as (
+            select project_id, count(*) 
+            from dob_match
+            group by project_id
+            having count(*) > 1),
+        multimatch_w_cluster as (
+            select *
+            from dob_match
+            where project_id in (
+                select project_id
+                FROM multimatch)
+            order by cluster_id),
+        formated as (
+            select 
+                b.source, 
+                b.project_id,
+                b.project_name,
+                b.project_status,
+                b.inactive,
+                b.project_type,
+                b.date,
+                b.date_type,
+                null as timeline,
+                b.dcp_projectcompleted,
+                b.number_of_units,
+                a.cluster_id,
+                a.sub_cluster_id,
+                b.geom
+        from multimatch_w_cluster a
+        left join dcp_housing b
+        on a.project_id = b.project_id)
+        (select * from {reviewed_clusters})
+        union
+        (select * from formated);''', build_engine)
