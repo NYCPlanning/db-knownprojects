86dc2e3e645f825e2119f270cae3e9b707595c4e
sptkl <caobaiyue@gmail.com>
2020-04-08 16:01:30 +0000

    upload dob match to carto
    Former-commit-id: 6914f248d6a7a21456efb4753cdab63288972d37

--- knownprojects_build/03.2_housing.sh
+++ knownprojects_build/03.2_housing.sh
@@ -16,3 +16,16 @@ docker run --rm\
 
 psql $BUILD_ENGINE -f sql/combine.sql
 psql $BUILD_ENGINE -f sql/dob_match.sql
+
+docker run --rm\
+    -v $(pwd):/home/knownprojects_build\
+    -w /home/knownprojects_build\
+    -e EDM_DATA=$EDM_DATA\
+    -e RECIPE_ENGINE=$RECIPE_ENGINE\
+    -e BUILD_ENGINE=$BUILD_ENGINE\
+    -e CARTO_USERNAME=$CARTO_USERNAME\
+    -e CARTO_APIKEY=$CARTO_APIKEY\
+    python:3.7-slim sh -c "
+        pip3 install -r python/requirements.txt; 
+        python3 python/upload_dob_match_to_carto.py"
+
