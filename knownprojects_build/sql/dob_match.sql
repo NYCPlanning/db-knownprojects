5169a59b7e40905af2d43aa560a9e601bdf4d1cb
sptkl <caobaiyue@gmail.com>
2020-04-28 17:00:06 +0000

    exclude negative alterations

--- knownprojects_build/sql/dob_match.sql
+++ knownprojects_build/sql/dob_match.sql
@@ -29,6 +29,8 @@ filtered_dcp_housing_proj as (
     WHERE a.project_type <> 'Demolition'
     AND a.project_status <> 'Withdrawn'
     AND b.units_prop::int > 0
+    AND (a.project_type <> 'Alteration'
+        and a.number_of_units::integer > 0)
 ),
 matches as (
     SELECT 
@@ -85,8 +87,7 @@ multimatch as (
 multimatchcluster as (
     select distinct development_id
     from combined_dob
-    where project_id in (select project_id from multimatch)
-)
+    where project_id in (select project_id from multimatch))
 select *,
     (case when project_id in 
 	 (select project_id from multimatch) and source='DOB' then 1 
