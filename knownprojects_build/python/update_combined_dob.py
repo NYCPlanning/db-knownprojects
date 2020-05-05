41616e612b58a7218bb2fd160fd2b6ab9ad148fb
mgraber <molly.graber@me.com>
2020-05-05 18:34:14 +0000

    Commit missing typo fixes from PR #99
    Former-commit-id: 7e0d26e00d3d4b6d2add5f113dad27e6233361d3

--- knownprojects_build/python/update_combined_dob.py
+++ knownprojects_build/python/update_combined_dob.py
@@ -4,13 +4,13 @@ import pandas as pd
 import numpy as np
 import os
 
-year = 'test'
+year = '2020'
 
 
 print(f"Making kpdb with {year} reviewed DOB data...")
 sql_make_kpdb = f'''
-    CREATE SCHEMA IF NOT EXIST kpdb;
-
+    CREATE SCHEMA IF NOT EXISTS kpdb;
+    DROP TABLE kpdb."{year}";
     CREATE TABLE kpdb."{year}" as (
         SELECT 
         source, record_id::text, record_name, 
@@ -32,20 +32,20 @@ sql_make_kpdb = f'''
         null as project_id,
         null as units_net,
         inactive, geom
-    from dcb_housing_proj)
+    from dcp_housing_proj)
 '''
 print(f"\n\nCombining DOB and non-DOB data into kpdb.{year}...")
 build_engine.execute(sql_make_kpdb)
 
 sql_update_dob = f'''
 UPDATE kpdb."{year}" a
-    SET project_id = b.project_id,
+    SET project_id = b.project_id
     FROM reviewed_dob_match."{year}" b
     WHERE a.source = 'DOB'
     AND b.source = 'DOB'
     AND a.record_id = b.record_id
     AND a.record_name = b.record_name
-    AND b.incorrect_match = 0;
+    AND b.incorrect_match = '0';
 '''
 print(f"Updating kpdb.{year} with DOB-review results...")
 build_engine.execute(sql_update_dob)
@@ -61,7 +61,7 @@ largest_project_id = int(pd.read_sql(sql_get_max, build_engine).values[0][0])
 sql_add_fields = f'''
 ALTER TABLE kpdb."{year}"
 ADD COLUMN IF NOT EXISTS within_5_years text,
-ADD COLUMN IF NOT EXISTS 5_to_10_years text,
+ADD COLUMN IF NOT EXISTS from_5_to_10_years text,
 ADD COLUMN IF NOT EXISTS after_10_years text,
 ADD COLUMN IF NOT EXISTS phasing_rationale text,
 ADD COLUMN IF NOT EXISTS phasing_assume_or_known text,
@@ -78,11 +78,10 @@ print(f"Creating IDs for stand-alone DOB records in kpdb.{year}...")
 df = pd.read_sql(f'SELECT * FROM kpdb."{year}"', build_engine)
 num_nulls = df[df['project_id'].isna()].shape[0]
 df.loc[df['project_id'].isna(),'project_id'] = pd.Series(range(largest_project_id, largest_project_id + num_nulls)).astype(str)
-df.loc[~df['project_id'].str.contains('-')],'project_id'] = df['project_id'] + '-1'
+df.loc[~df['project_id'].str.contains('-', na=False),'project_id'] = df['project_id'] + '-1'
 df.loc[df['units_net'].isna(),'units_net'] = df['units_gross']
 
 # Reformat numbers
-df['project_id'] = df.cluster_id.replace('\.0', '', regex=True)
 df['units_net'] = df.units_net.replace('\.0', '', regex=True)
 
 # Export to temporary table
