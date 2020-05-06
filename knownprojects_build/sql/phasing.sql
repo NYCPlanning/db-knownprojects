e810d3ba406bb4549ea7bc3903edb9dd40282c8c
Amanda Doyle <AmandaDoyle>
2020-05-06 21:54:47 +0000

    finalizing phasing logic

--- knownprojects_build/sql/phasing.sql
+++ knownprojects_build/sql/phasing.sql
@@ -1,46 +1,76 @@
 -- DOB
-UPDATE combined_ADtest
-SET    portion_built_by_2025 = CASE WHEN status = 'Permit issued' OR status = 'Filed' OR status LIKE 'In progress%' THEN 1 ELSE NULL END
-     , portion_built_by_2035 = CASE WHEN status <> 'Withdrawn' AND inactive = 1 THEN 1 ELSE NULL END
+UPDATE kpdb_gross."2020"
+SET    prop_within_5_years = CASE WHEN status = 'Permit issued' OR status = 'Filed' OR status LIKE 'In progress%' THEN 1 ELSE NULL END
+     , prop_5_to_10_years = CASE WHEN status <> 'Withdrawn' AND inactive = 1 THEN 1 ELSE NULL END
      , phasingknown = 0
 WHERE source = 'DOB';
 -- HPD Projected Closings
-UPDATE combined_ADtest
-SET    portion_built_by_2025 = CASE WHEN date_part('year',age(to_date((CONCAT(RIGHT(date,4)::numeric+3,'-06-30')),'YYYY-MM-DD'),CURRENT_DATE)) <= 5 THEN 1 ELSE NULL END
-     , portion_built_by_2035 = CASE WHEN date_part('year',age(to_date((CONCAT(RIGHT(date,4)::numeric+3,'-06-30')),'YYYY-MM-DD'),CURRENT_DATE)) > 5 AND date_part('year',age(to_date((CONCAT(RIGHT(date,4)::numeric+3,'-06-30')),'YYYY-MM-DD'),CURRENT_DATE)) <= 10 THEN 1 ELSE NULL END
-     , portion_built_by_2055 = CASE WHEN date_part('year',age(to_date((CONCAT(RIGHT(date,4)::numeric+3,'-06-30')),'YYYY-MM-DD'),CURRENT_DATE)) > 10 THEN 1 ELSE NULL END
+UPDATE kpdb_gross."2020"
+SET    prop_within_5_years = CASE WHEN date_part('year',age(to_date((CONCAT(RIGHT(date,4)::numeric+3,'-06-30')),'YYYY-MM-DD'),CURRENT_DATE)) <= 5 THEN 1 ELSE NULL END
+     , prop_5_to_10_years = CASE WHEN date_part('year',age(to_date((CONCAT(RIGHT(date,4)::numeric+3,'-06-30')),'YYYY-MM-DD'),CURRENT_DATE)) > 5 AND date_part('year',age(to_date((CONCAT(RIGHT(date,4)::numeric+3,'-06-30')),'YYYY-MM-DD'),CURRENT_DATE)) <= 10 THEN 1 ELSE NULL END
+     , prop_after_10_years = CASE WHEN date_part('year',age(to_date((CONCAT(RIGHT(date,4)::numeric+3,'-06-30')),'YYYY-MM-DD'),CURRENT_DATE)) > 10 THEN 1 ELSE NULL END
      , phasingknown = 1
 WHERE source = 'HPD Projected Closings';
 -- HPD RFPs
-UPDATE combined_ADtest
-SET    portion_built_by_2025 = 1
+UPDATE kpdb_gross."2020"
+SET    prop_within_5_years = 1
      , phasingknown = 1
 WHERE source = 'HPD RFPs';
 -- EDC
-UPDATE combined_ADtest
-SET    portion_built_by_2025 = CASE WHEN date::numeric <= date_part('year', CURRENT_DATE)+5 THEN 1 ELSE NULL END
-     , portion_built_by_2035 = CASE WHEN date::numeric > date_part('year', CURRENT_DATE)+5 AND date::numeric <= date_part('year', CURRENT_DATE)+10 THEN 1 ELSE NULL END
-     , portion_built_by_2055 = CASE WHEN date::numeric > date_part('year', CURRENT_DATE)+10 THEN 1 ELSE NULL END
+UPDATE kpdb_gross."2020"
+SET    prop_within_5_years = CASE WHEN date::numeric <= date_part('year', CURRENT_DATE)+5 THEN 1 ELSE NULL END
+     , prop_5_to_10_years = CASE WHEN date::numeric > date_part('year', CURRENT_DATE)+5 AND date::numeric <= date_part('year', CURRENT_DATE)+10 THEN 1 ELSE NULL END
+     , prop_after_10_years = CASE WHEN date::numeric > date_part('year', CURRENT_DATE)+10 THEN 1 ELSE NULL END
      , phasingknown = 1
 WHERE source = 'EDC Projected Projects';
 -- DCP Application
-UPDATE combined_ADtest
-SET   portion_built_by_2035 = 1 
+UPDATE kpdb_gross."2020"
+SET   prop_5_to_10_years = 1 
      , phasingknown = 0
 WHERE source = 'DCP Application' AND status <> 'Record Closed';
--- Neighborhood Study Projected Development Sites (work in progress)
-WITH counting AS (
-    SELECT RIGHT('9/7/2017',4)::numeric+3 as startyear,
-        RIGHT('9/7/2017',4)::numeric+10 as endyear
-)
+-- Neighborhood Study Projected Development Sites
+WITH years as (
+     SELECT record_id, generate_series(LEFT(date,4)::numeric+3, LEFT(date,4)::numeric+9) as yearspan
+     FROM dcp_n_study_projected_proj),
+yeargroups as (
+     SELECT record_id,
+          COUNT(*) as total,
+          sum(case when yearspan <= date_part('year', CURRENT_DATE)+5 then 1 else 0 end)::numeric AS fiveyear,
+          sum(case when yearspan > date_part('year', CURRENT_DATE)+5 AND yearspan <= date_part('year', CURRENT_DATE)+10 then 1 else 0 end)::numeric AS tenyear,
+          sum(case when yearspan > date_part('year', CURRENT_DATE)+10 then 1 else 0 end)::numeric AS tenyearplus
+     FROM years
+     GROUP BY record_id)
+UPDATE kpdb_gross."2020" a
+SET   prop_within_5_years = round(fiveyear/total,2)
+     ,prop_5_to_10_years = round(tenyear/total,2)
+     ,prop_after_10_years = round(tenyearplus/total,2)
+FROM yeargroups b
+WHERE a.record_id=b.record_id
+AND source = 'Neighborhood Study Projected Development Sites'
+AND record_name <> 'East New York';
 
-UPDATE combined_ADtest
-SET   portion_built_by_2035 = 
-     , phasingknown = 0
-WHERE source = 'Neighborhood Study Projected Development Sites';
+WITH years as (
+     SELECT record_id, generate_series(LEFT(date,4)::numeric+3, LEFT(date,4)::numeric+14) as yearspan
+     FROM dcp_n_study_projected_proj),
+yeargroups as (
+     SELECT record_id,
+          COUNT(*) as total,
+          sum(case when yearspan <= date_part('year', CURRENT_DATE)+5 then 1 else 0 end)::numeric AS fiveyear,
+          sum(case when yearspan > date_part('year', CURRENT_DATE)+5 AND yearspan <= date_part('year', CURRENT_DATE)+10 then 1 else 0 end)::numeric AS tenyear,
+          sum(case when yearspan > date_part('year', CURRENT_DATE)+10 then 1 else 0 end)::numeric AS tenyearplus
+     FROM years
+     GROUP BY record_id)
+UPDATE kpdb_gross."2020" a
+SET   prop_within_5_years = round(fiveyear/total,2)
+     ,prop_5_to_10_years = round(tenyear/total,2)
+     ,prop_after_10_years = round(tenyearplus/total,2)
+FROM yeargroups b
+WHERE a.record_id=b.record_id
+AND source = 'Neighborhood Study Projected Development Sites'
+AND record_name = 'East New York';
 -- Future Neighborhood Studies
-UPDATE combined_ADtest
-SET   portion_built_by_2035 = CASE WHEN record_name LIKE 'Gowanus%' THEN round(1/3::numeric,2) ELSE .5 END
-      portion_built_by_2055 = CASE WHEN record_name LIKE 'Gowanus%' THEN round(2/3::numeric,2) ELSE .5 END
+UPDATE kpdb_gross."2020"
+SET   prop_5_to_10_years = CASE WHEN record_name LIKE 'Gowanus%' THEN round(1/3::numeric,2) ELSE .5 END
+      prop_after_10_years = CASE WHEN record_name LIKE 'Gowanus%' THEN round(2/3::numeric,2) ELSE .5 END
      , phasingknown = 0
 WHERE source = 'Future Neighborhood Studies';
