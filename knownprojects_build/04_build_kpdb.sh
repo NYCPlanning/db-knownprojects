8259a22a1fdc9e6837f8169e1b15a8042352ffef
Molly Graber <molly.graber@me.com>
2020-05-07 13:04:13 -0400

    Map non-ZAP status values (#145)
    * Map non-ZAP status values
    
    * adding ZAP status normalization
    
    Co-authored-by: sptkl <caobaiyue@gmail.com>

--- knownprojects_build/04_build_kpdb.sh
+++ knownprojects_build/04_build_kpdb.sh
@@ -17,4 +17,5 @@ docker run --rm\
 psql $BUILD_ENGINE -f sql/add_remaining_n_study.sql
 psql $BUILD_ENGINE -f sql/phasing.sql
 psql $BUILD_ENGINE -f sql/phasing_counts.sql
+psql $BUILD_ENGINE -f sql/normalize_status.sql
 psql $BUILD_ENGINE -f sql/assign_boolean.sql
