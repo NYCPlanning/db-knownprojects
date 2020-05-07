5c5f3a9992824174c6e696f32d10fa0eef71a356
mgraber <molly.graber@me.com>
2020-05-07 14:59:32 +0000

    Treat null proportions as 0 and rount to ints

--- knownprojects_build/sql/phasing_counts.sql
+++ knownprojects_build/sql/phasing_counts.sql
@@ -1,5 +1,4 @@
 UPDATE kpdb."2020"
-SET within_5_years = prop_within_5_years::decimal * units_net::decimal,
-    from_5_to_10_years = prop_5_to_10_years::decimal * units_net::decimal,
-    after_10_years = prop_after_10_years::decimal * units_net::decimal,
-    phasing_assume_or_known = 'Assumed';
+SET within_5_years = ROUND(COALESCE(prop_within_5_years::decimal,0) * units_net::decimal),
+    from_5_to_10_years = ROUND(COALESCE(prop_5_to_10_years::decimal,0) * units_net::decimal),
+    after_10_years = ROUND(COALESCE(prop_after_10_years::decimal,0) * units_net::decimal);
