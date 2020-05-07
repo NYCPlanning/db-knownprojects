a5ce71e440ccf4fcbb675a3b725f8050d21c255c
mgraber <molly.graber@me.com>
2020-05-07 12:40:16 +0000

    Multiply prop phasing by units_net

---
+++ knownprojects_build/sql/phasing_counts.sql
@@ -0,0 +1,4 @@
+UPDATE kpdb."2020"
+SET within_5_years = prop_within_5_years::decimal * units_net::decimal,
+    from_5_to_10_years = prop_5_to_10_years::decimal * units_net::decimal,
+    after_10_years = prop_after_10_years::decimal * units_net::decimal;
