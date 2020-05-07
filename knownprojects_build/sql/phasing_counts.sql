3a6280a18e8854d45fc2922a8e58f56010b6ca6c
mgraber <molly.graber@me.com>
2020-05-07 12:42:53 +0000

    Add assumed phasing flag as default

--- knownprojects_build/sql/phasing_counts.sql
+++ knownprojects_build/sql/phasing_counts.sql
@@ -1,4 +1,5 @@
 UPDATE kpdb."2020"
 SET within_5_years = prop_within_5_years::decimal * units_net::decimal,
     from_5_to_10_years = prop_5_to_10_years::decimal * units_net::decimal,
-    after_10_years = prop_after_10_years::decimal * units_net::decimal;
+    after_10_years = prop_after_10_years::decimal * units_net::decimal,
+    phasing_assume_or_known = 'Assumed';
