92dd541a4bdad98f67059160720e2c8be6a13cf1
sptkl <caobaiyue@gmail.com>
2020-05-12 16:26:24 +0000

    pick boro with largest overlapping area
    Former-commit-id: 67e68e8ef2fd6342776ea1c471bb68a230cf2dd7

--- knownprojects_build/sql/assign_boro.sql
+++ knownprojects_build/sql/assign_boro.sql
@@ -1,4 +1,18 @@
+with pick_boro as (
+	select 
+		a.record_id, 
+		b.boroname, 
+		st_area(st_intersection(st_makevalid(a.geom), b.wkb_geometry)) as area
+	from kpdb."2020" a, dcp_boroboundaries_wi b
+	where  st_intersects(st_makevalid(a.geom), b.wkb_geometry)),
+pick_boro_ordered as (
+	select 
+		record_id, 
+		boroname, area, 
+		ROW_NUMBER() OVER(PARTITION BY record_id ORDER BY area DESC) AS row_number
+	from pick_boro
+	order by record_id, row_number)
 UPDATE kpdb."2020" a
 SET borough = b.boroname
-FROM dcp_boroboundaries_wi b
-WHERE st_intersects(st_makevalid(a.geom), b.wkb_geometry);
+FROM pick_boro_ordered b
+WHERE a.record_id = b.record_id and b.row_number = 1;
