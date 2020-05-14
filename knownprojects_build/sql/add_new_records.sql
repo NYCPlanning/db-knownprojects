2bba04a3dc5c81adafc7757f06fd3138ec66b423
mgraber <molly.graber@me.com>
2020-05-14 14:04:27 +0000

    Add new DOB records and assign project_ids
    Former-commit-id: 5eff9d11d02682fcbcbeba9e06f3a9c2a09ffdd5

--- knownprojects_build/sql/add_new_records.sql
+++ knownprojects_build/sql/add_new_records.sql
@@ -9,7 +9,7 @@ INSERT INTO kpdb."2020"
         date,
         date_type,
         units_gross,
-        NULL as units_net,
+        coalesce(units_net::integer, units_gross::integer) as units_net,
         NULL as prop_within_5_years,
         NULL as prop_5_to_10_years,
         NULL as prop_after_10_years,
@@ -31,4 +31,25 @@ INSERT INTO kpdb."2020"
     AND record_id NOT IN (SELECT DISTINCT record_id
                         FROM kpdb."2020"));
 
-		
+-- Add previously filtered ZAP based on kpdb_corrections
+
+
+-- Assign stand-alone project IDs for new additions
+WITH
+max_proj AS (
+	SELECT max(split_part(project_id, '-', 1)::integer) AS max_proj_number
+    FROM kpdb."2020"),
+tmp AS (
+	SELECT
+        a.source,
+        a.record_id,
+		a.record_name,
+        (ROW_NUMBER() OVER (ORDER BY record_id) + b.max_proj_number)::text||'-1' as project_id
+    FROM kpdb."2020" a, max_proj b
+    WHERE a.project_id IS NULL)
+UPDATE kpdb_gross."2020" a
+	SET project_id = b.project_id,
+	FROM tmp b
+	WHERE a.source = b.source
+	AND a.record_id = b.record_id
+	AND a.record_name = b.record_name;
