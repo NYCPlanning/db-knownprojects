aafe20d0299410edf2ffe8ee2a8e1c038e7f06ab
mgraber <molly.graber@me.com>
2020-05-22 16:11:56 +0000

    Change table names
    Former-commit-id: 8be90541023c500dcf95d6c282c3b4f394c35544

--- knownprojects_build/sql/apply_corrections.sql
+++ knownprojects_build/sql/apply_corrections.sql
@@ -4,14 +4,14 @@ SET old_value = nullif(old_value, ' '),
 
 -- Remove records
 DELETE FROM kpdb."2020" a
-USING kpdb_corrections b
+USING kpdb_corrections.latest b
 WHERE b.field = 'remove'
 AND a.record_id = b.record_id;
 
 -- Correct project_id
 UPDATE kpdb."2020" a
 SET project_id = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'project_id'
 AND a.record_id = b.record_id 
 AND a.project_id = b.old_value;
@@ -19,7 +19,7 @@ AND a.project_id = b.old_value;
 -- Correct record_name
 UPDATE kpdb."2020" a
 SET record_name = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'record_name'
 AND a.record_id = b.record_id 
 AND a.record_name = b.old_value;
@@ -27,7 +27,7 @@ AND a.record_name = b.old_value;
 -- Correct borough, if new borough is in correct form
 UPDATE kpdb."2020" a
 SET borough = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'borough'
 AND a.record_id = b.record_id 
 AND a.borough = b.old_value
@@ -36,7 +36,7 @@ AND b.new_value IS IN ('Manhattan','Brooklyn','Staten Island','Queens','Bronx');
 -- Correct status
 UPDATE kpdb."2020" a
 SET status = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'status'
 AND a.record_id = b.record_id 
 AND a.status = b.old_value;
@@ -44,7 +44,7 @@ AND a.status = b.old_value;
 -- Correct type
 UPDATE kpdb."2020" a
 SET type = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'type'
 AND a.record_id = b.record_id 
 AND a.type = b.old_value;
@@ -52,7 +52,7 @@ AND a.type = b.old_value;
 -- Correct date, if formatted correctly
 UPDATE kpdb."2020" a
 SET date = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'date'
 AND a.record_id = b.record_id 
 AND a.date = b.old_value
@@ -61,7 +61,7 @@ AND b.new_value LIKE '____/__/__';
 -- Correct date_type
 UPDATE kpdb."2020" a
 SET date_type = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'date_type'
 AND a.record_id = b.record_id 
 AND a.date_type = b.old_value;
@@ -69,7 +69,7 @@ AND a.date_type = b.old_value;
 -- Correct units_gross
 UPDATE kpdb."2020" a
 SET units_gross = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'units_gross'
 AND a.record_id = b.record_id 
 AND a.units_gross = b.old_value;
@@ -77,7 +77,7 @@ AND a.units_gross = b.old_value;
 -- Correct prop_within_5_years, checking that new value is between zero & one
 UPDATE kpdb."2020" a
 SET prop_within_5_years = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'prop_within_5_years'
 AND a.record_id = b.record_id 
 AND a.prop_within_5_years = b.old_value
@@ -86,7 +86,7 @@ AND b.new_value::numeric <= 1 AND b.new_value::numeric >= 0;
 -- Correct prop_5_to_10_years, checking that new value is between zero & one
 UPDATE kpdb."2020" a
 SET prop_5_to_10_years = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'prop_5_to_10_years'
 AND a.record_id = b.record_id 
 AND a.prop_5_to_10_years = b.old_value
@@ -95,7 +95,7 @@ AND b.new_value::numeric <= 1 AND b.new_value::numeric >= 0;
 -- Correct prop_after_10_years, checking that new value is between zero & one
 UPDATE kpdb."2020" a
 SET prop_after_10_years = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'prop_after_10_years'
 AND a.record_id = b.record_id 
 AND a.prop_after_10_years = b.old_value
@@ -104,7 +104,7 @@ AND b.new_value::numeric <= 1 AND b.new_value::numeric >= 0;
 -- Correct phasing_rationale
 UPDATE kpdb."2020" a
 SET phasing_rationale = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'phasing_rationale'
 AND a.record_id = b.record_id 
 AND a.phasing_rationale = b.old_value;
@@ -112,7 +112,7 @@ AND a.phasing_rationale = b.old_value;
 -- Correct phasing_known, if new value is valid
 UPDATE kpdb."2020" a
 SET phasing_known = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'phasing_known'
 AND a.record_id = b.record_id 
 AND a.phasing_known = b.old_value
@@ -121,7 +121,7 @@ AND b.new_value::text IS IN ('0','1');
 -- Correct nycha
 UPDATE kpdb."2020" a
 SET nycha = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'nycha'
 AND a.record_id = b.record_id 
 AND a.nycha = b.old_value
@@ -130,7 +130,7 @@ AND b.new_value::text IS IN ('0','1');
 -- Correct gq
 UPDATE kpdb."2020" a
 SET gq = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'gq'
 AND a.record_id = b.record_id 
 AND a.gq = b.old_value
@@ -139,7 +139,7 @@ AND b.new_value::text IS IN ('0','1');
 -- Correct senior_housing
 UPDATE kpdb."2020" a
 SET senior_housing = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'senior_housing'
 AND a.record_id = b.record_id 
 AND a.senior_housing = b.old_value
@@ -148,7 +148,7 @@ AND b.new_value::text IS IN ('0','1');
 -- Correct assisted_living
 UPDATE kpdb."2020" a
 SET assisted_living = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'assisted_living'
 AND a.record_id = b.record_id 
 AND a.assisted_living = b.old_value
@@ -157,7 +157,7 @@ AND b.new_value::text IS IN ('0','1');
 -- Correct inactive
 UPDATE kpdb."2020" a
 SET inactive = b.new_value
-FROM kpdb_corrections b
+FROM kpdb_corrections.latest b
 WHERE b.field = 'inactive'
 AND a.record_id = b.record_id 
 AND a.inactive = b.old_value
