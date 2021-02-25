b1a6afdc373d32cb268d0b4aebce952fbd2ef349
mgraber <mgraber@planning.nyc.gov>
2021-02-25 15:24:05 +0000

    Add filters to DOB jobs from dob_match.sql

--- knownprojects_build/sql/out_dcp_housing.sql
+++ knownprojects_build/sql/out_dcp_housing.sql
@@ -12,6 +12,12 @@ CREATE TEMP TABLE dcp_housing as (
         date_complete,
         job_inactive
     FROM dcp_housing.:"VERSION"  
+    WHERE job_type <> 'Demolition'
+    AND job_status <> '9. Withdrawn'
+    AND classa_net::integer <> 0
+    AND classa_prop::integer > 0
+    AND NOT (job_type = 'Alteration'
+        AND classa_net::integer <= 0)
 );
 
 \COPY dcp_housing TO PSTDOUT DELIMITER ',' CSV HEADER;
