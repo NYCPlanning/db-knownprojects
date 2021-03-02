945471146a8b7d928ecd0940210d55fe6d3e8bdd
mgraber <mgraber@planning.nyc.gov>
2021-03-02 15:43:29 +0000

    Change status format

--- knownprojects_build/_sql/dcp_housing.sql
+++ knownprojects_build/_sql/dcp_housing.sql
@@ -1,6 +1,10 @@
 DROP TABLE IF EXISTS dcp_housing_poly;
 WITH 
--- Prior to geom steps, filter to relevant DOB jobs
+/* Prior to geom steps, filter to relevant DOB jobs:
+Excludes demolitions, withdrawn jobs, jobs with
+no residential unit change, alterations removing units,
+and jobs where the proposed residential units are 0.
+*/
 dcp_housing_filtered AS (
 	SELECT *
     FROM dcp_housing a
@@ -19,10 +23,7 @@ bbl_join AS (
 		a.job_number,
 		a.bbl,
 		a.wkb_geometry as point_geom,
-		(CASE
-			WHEN ST_IsValid(b.wkb_geometry) THEN b.wkb_geometry
-			ELSE ST_MakeValid(b.wkb_geometry) 
-		END) as bbl_join_geom
+		b.wkb_geometry as bbl_join_geom
 	FROM dcp_housing_filtered a
 	LEFT JOIN dcp_mappluto b
     ON a.bbl = b.bbl::bigint::text
@@ -35,13 +36,10 @@ spatial_join AS(
 		a.job_number,
 		a.bbl,
 		a.point_geom,
-		(CASE
-			WHEN ST_IsValid(b.wkb_geometry) THEN b.wkb_geometry
-			ELSE ST_MakeValid(b.wkb_geometry) 
-		END) as spatial_join_geom
+		b.wkb_geometry as spatial_join_geom
 	FROM bbl_join a
 	JOIN dcp_mappluto b
-	ON ST_Within(a.point_geom, ST_MakeValid(b.wkb_geometry))
+	ON ST_Intersects(a.point_geom, b.wkb_geometry)
 	WHERE a.bbl_join_geom IS NULL AND a.point_geom IS NOT NULL
 ),
 -- Combine into a single geom lookup
@@ -66,7 +64,7 @@ SELECT
 	a.job_number as record_id,
 	NULL as record_id_input,
 	a.address as record_name,
-	'DOB: '||a.job_status as status,
+	'DOB '||a.job_status as status,
 	a.job_type as type,
 	a.classa_net as units_net,
 	(CASE
@@ -90,15 +88,20 @@ SELECT
 
     -- Phasing
     (CASE 
-        WHEN a.job_status ~* '1|2|3' AND inactive <> '1' THEN 1 
+        WHEN a.job_status ~* '1. Filed Application|2. Approved Application|3. Permitted for Construction' 
+            AND a.job_inactive IS NULL THEN 1 
         ELSE NULL
     END) as prop_within_5_years,
     (CASE 
-        WHEN a.job_status <> '9. Withdrawn' AND inactive = '1' THEN 1 
+        WHEN a.job_inactive = 'Inactive' THEN 1 
         ELSE NULL
     END) as prop_5_to_10_years,
     NULL as prop_after_10_years,
     0 as phasing_known,
+    flag_nycha(a::text) as nycha,
+    flag_gq(a::text) as gq,
+    flag_senior_housing(a::text) as senior_housing,
+    flag_assisted_living(a::text) as assisted_living,
 	b.geom,
 	b.geom_source
 INTO dcp_housing_poly
