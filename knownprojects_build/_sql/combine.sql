551e8a649388602b34bd12c3badf65f11777833e
mgraber <mgraber@planning.nyc.gov>
2021-03-04 16:57:29 +0000

    Reorganize function and procedure creation
    Former-commit-id: c097351843978211c6f6f527d84b210f42e6bce9

--- knownprojects_build/_sql/combine.sql
+++ knownprojects_build/_sql/combine.sql
@@ -1,33 +1,12 @@
 /*
-FLAG FUNCTIONS
-*/
-CREATE OR REPLACE FUNCTION flag_assisted_living(stringy varchar) 
-RETURNS integer AS $$
-	SELECT (stringy ~* 
-    'ASSISTED LIVING')::integer;
-$$ LANGUAGE sql;
+DESCRIPTION:
+    
 
-CREATE OR REPLACE FUNCTION flag_senior_housing(stringy varchar) 
-RETURNS integer AS $$
-	SELECT (stringy ~* 
-    'SENIOR|ELDERLY| AIRS |A.I.R.S|CONTINUING CARE|NURSING| SARA |S.A.R.A')::integer;
-$$ LANGUAGE sql;
-
-CREATE OR REPLACE FUNCTION flag_gq(stringy varchar) 
-RETURNS integer AS $$
-	SELECT (stringy ~* 
-    'CORRECTIONAL|NURSING| MENTAL|DORMITOR|MILITARY|GROUP HOME|BARRACK')::integer;
-$$ LANGUAGE sql;
+INPUTS: 
 
 
-CREATE OR REPLACE FUNCTION flag_nycha(stringy varchar) 
-RETURNS integer AS $$
-	SELECT (stringy ~* 
-    'NYCHA|BTP|HOUSING AUTHORITY|NEXT GEN|NEXT-GEN|NEXTGEN|NEXTGEN')::integer;
-$$ LANGUAGE sql;
-
-/*
-SOURCE TABLE MAPPING
+OUTPUTS: 
+    
 */
 DROP TABLE IF EXISTS _combined;
 WITH 
