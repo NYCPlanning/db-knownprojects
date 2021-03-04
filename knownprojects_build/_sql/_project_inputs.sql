f76e61a1fa9619078f61e431d4d15a2e812a66ec
mgraber <mgraber@planning.nyc.gov>
2021-03-04 19:27:21 +0000

    Add two project input scripts -- _project_inputs is scaffolding

---
+++ knownprojects_build/_sql/_project_inputs.sql
@@ -0,0 +1,15 @@
+/*
+DESCRIPTION:
+    Create initial table of project inputs (groups of record_ids that refer to the
+    same project)
+
+INPUTS: 
+
+	_combined(
+
+	)
+OUTPUTS: 
+    _project_inputs(
+        
+    )
+*/
