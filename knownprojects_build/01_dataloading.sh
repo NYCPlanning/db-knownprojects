fb7f8affe532ac28fd515543c44593c7537c3830
sptkl <caobaiyue@gmail.com>
2021-03-05 18:30:40 +0000

    adding data loading for zap and mappluto

--- knownprojects_build/01_dataloading.sh
+++ knownprojects_build/01_dataloading.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 source config.sh
 
-# Load source data
+Load source data
 for f in $(ls data/processed)
 do 
-    psql $BUILD_ENGINE -f data/processed/$f
+    psql $BUILD_ENGINE -f data/processed/$f &
 done
+wait
+
+# Load ZAP tables
+import_private dcp_projects &
+import_private dcp_projectactions &
+import_private dcp_projectbbls
+
+# Load other tables
+import_public dcp_mappluto_wi
--- knownprojects_build/config.sh
+++ knownprojects_build/config.sh
@@ -3,3 +3,20 @@ if [ -f .env ]
 then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
+
+function import_private {
+  name=$1
+  version=${2:-latest} #default version to latest
+  mc cp spaces/edm-recipes/datasets/$name/$version/$name.sql $name.sql
+  psql $BUILD_ENGINE -f $name.sql
+  rm $name.sql
+}
+
+function import_public {
+  name=$1
+  version=${2:-latest}
+  version=$(curl -s https://nyc3.digitaloceanspaces.com/edm-recipes/datasets/$name/$version/config.json | jq -r '.dataset.version')
+  curl -O https://nyc3.digitaloceanspaces.com/edm-recipes/datasets/$name/$version/$name.sql
+  psql $BUILD_ENGINE -f $name.sql
+  rm $name.sql
+}
