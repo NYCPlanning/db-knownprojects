fdd673154755243e8bf66834fd2bfce2dbd93d22
SPTKL <caobaiyue@gmail.com>
2021-03-05 13:21:18 -0500

    dataloading for sourcce datasets (#237)

--- knownprojects_build/01_dataloading.sh
+++ knownprojects_build/01_dataloading.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 source config.sh
 
-docker run --rm\
-            -v `pwd`:/home/knownprojects_build\
-            -w /home/knownprojects_build\
-            -e EDM_DATA=$EDM_DATA\
-            -e RECIPE_ENGINE=$RECIPE_ENGINE\
-            -e BUILD_ENGINE=$BUILD_ENGINE\
-            sptkl/cook:latest bash -c "python3 python/dataloading.py"
-
-# postgres version = 11
-pg_dump $ZAP_ENGINE -t project_geoms -O -c | psql $BUILD_ENGINE
-pg_dump $ZAP_ENGINE -t dcp_projectaction -O -c | psql $BUILD_ENGINE
-pg_dump $ZAP_ENGINE -t dcp_projectbbl -O -c | psql $BUILD_ENGINE
-pg_dump $ZAP_ENGINE -t dcp_project -O -c | psql $BUILD_ENGINE
+# Load source data
+for f in $(ls data/processed)
+do 
+    psql $BUILD_ENGINE -f data/processed/$f
+done
