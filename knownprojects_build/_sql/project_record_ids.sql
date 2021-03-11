d2f5033284f4135d9ac1bf2b24dd336cbd7b1b0c
mgraber <mgraber@planning.nyc.gov>
2021-03-11 19:12:19 +0000

    Do not include contextual class b records in projects
    Former-commit-id: b3b101be12cf37a037564c468b3bf6bd6e750a82

--- knownprojects_build/_sql/project_record_ids.sql
+++ knownprojects_build/_sql/project_record_ids.sql
@@ -45,6 +45,10 @@ dob_matches AS(
 		project_record_ids
 	FROM dob_review
 	WHERE source = 'DOB'
+	AND units_gross::integer <> 0
+    AND classa_prop::integer > 0
+    AND NOT (type = 'Alteration'
+        AND units_gross::integer <= 0)
 ),
 matches_to_remove AS(
 	SELECT 
