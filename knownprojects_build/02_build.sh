03136b8a9a240016f7b5a04c44b5d3e6ccc6d974
SPTKL <caobaiyue@gmail.com>
2021-03-15 12:21:15 -0400

    applying corrections (#247)
    * applying corrections
    
    * only correct valid fields
    
    Former-commit-id: bc06d1f155a647a337ef917e7c7f60eef53192c2

--- knownprojects_build/02_build.sh
+++ knownprojects_build/02_build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 source config.sh
 
-# Create functions -- these get used to create flags
+# Create functions and procedures
 psql $BUILD_ENGINE -1 -f _sql/_functions.sql
+psql $BUILD_ENGINE -1 -f _sql/_procedures.sql
 
 # Map source data
 psql $BUILD_ENGINE -1 -f _sql/dcp_application.sql
 psql $BUILD_ENGINE -1 -f _sql/dcp_housing.sql
 psql $BUILD_ENGINE -1 -f _sql/combine.sql
+psql $BUILD_ENGINE -1 -c "CALL apply_correction('_combined');"
 
 # Find and matches between non-DOB sources
 psql $BUILD_ENGINE -1 -f _sql/_project_record_ids.sql
