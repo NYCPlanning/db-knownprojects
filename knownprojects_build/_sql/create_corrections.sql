573c8e0338706ee682033cfbd805ba82a1704859
SPTKL <caobaiyue@gmail.com>
2021-03-15 10:53:07 -0400

    condense documentation (#244)
    * condense documentation
    1. simplify documentation and over documentation
    2. dcp_mappluto -> dcp_mappluto_wi
    3. move all corrections table import into create_corrections.sql
    
    * adding two other corrections table create statement
    
    * Update correct_projects.sql
    
    * Update create_corrections.sql
    
    * Update create_corrections.sql
    
    * adding 3 correction files
    
    Former-commit-id: 7bfb9ca8017eadaadbabc92129d336d5c0a68100

---
+++ knownprojects_build/_sql/create_corrections.sql
@@ -0,0 +1,34 @@
+-- first round of corrections -> project to project
+DROP TABLE IF EXISTS corrections_project;
+CREATE TABLE corrections_project(
+    record_id text,
+    action text,
+    record_id_match text
+);
+
+\COPY corrections_project FROM 'data/corrections/corrections_project.csv' DELIMITER ',' CSV HEADER;
+
+-- second round of corrections -> dob to project
+DROP TABLE IF EXISTS corrections_dob;
+CREATE TABLE corrections_dob(
+    record_id text,
+    action text,
+    record_id_dob text
+);
+
+\COPY corrections_dob FROM 'data/corrections/corrections_dob.csv' DELIMITER ',' CSV HEADER;
+
+-- main corrections table -> all other fields
+DROP TABLE IF EXISTS corrections_main;
+CREATE TABLE corrections_main(
+    record_id text,
+    field text,
+    old_value text,
+    new_value text,
+    editor text,
+    date text,
+    record_name text,
+    notes text
+);
+
+\COPY corrections_main FROM 'data/corrections/corrections_main.csv' DELIMITER ',' CSV HEADER;
