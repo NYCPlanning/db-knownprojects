15f9f3a8aad80a1fc07bbcd97d313be1ab1929ff
SPTKL <caobaiyue@gmail.com>
2021-03-15 16:36:12 -0400

    add and remove records for dcp_applications using corrections_main (#248)

--- knownprojects_build/02_build.sh
+++ knownprojects_build/02_build.sh
@@ -10,14 +10,17 @@ psql $BUILD_ENGINE -1 -f _sql/dcp_application.sql
 psql $BUILD_ENGINE -1 -f _sql/dcp_housing.sql
 psql $BUILD_ENGINE -1 -f _sql/combine.sql
 psql $BUILD_ENGINE -1 -c "CALL apply_correction('_combined');"
+psql $BUILD_ENGINE -c "VACUUM ANALYZE _combined;"
 
 # Find and matches between non-DOB sources
 psql $BUILD_ENGINE -1 -f _sql/_project_record_ids.sql
 
 # Apply corrections to reassign records to projects
 psql $BUILD_ENGINE -1 -f _sql/correct_projects.sql
+psql $BUILD_ENGINE -c "VACUUM ANALYZE _project_record_ids;"
 
 # Find matches between DOB and non-DOB sources
 psql $BUILD_ENGINE -1 -f _sql/dob_match.sql
 
 psql $BUILD_ENGINE -1 -f _sql/project_record_ids.sql
+psql $BUILD_ENGINE -c "VACUUM ANALYZE project_record_ids;"
