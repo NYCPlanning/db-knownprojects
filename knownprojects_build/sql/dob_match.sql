dcb9b2ad37c42ffb92e05a99d5691b06a565c87f
SPTKL <caobaiyue@gmail.com>
2021-03-16 13:51:43 -0400

    rename directory and remove old files (#263)

--- knownprojects_build/sql/dob_match.sql
+++ knownprojects_build/sql/dob_match.sql
@@ -1,101 +1,117 @@
--- Create matches, containing inner spatial join of DOB and non-DOB given time constraint
--- Add KPDB-relevant DOB jobs to the combination of all other sources. This is called combined_dob
--- Get a list of relevant clusters. These clusters are the ones that contain overlap between DOB and non-DOB
--- Find cases where a DOB job matches with more than one non-DOB job, and flag for review.
-
-drop table if exists dob_review;
-with 
-filtered_dcp_housing_proj as (
-    SELECT a.source, 
-        a.record_id::text, 
-        a.record_name, 
-        a.status, 
-        a.type,
-        a.inactive,
-        a.units_gross::integer, 
-        a.date, 
-        a.date_type,
-        a.date_filed,
-        a.date_complete,  
-        a.dcp_projectcompleted,
-        null as portion_built_by_2025, 
-        null as portion_built_by_2035, 
-        null as portion_built_by_2055, 
-        a.geom,
-        b.units_prop
-    from dcp_housing_proj a
-    LEFT JOIN dcp_housing b
-    ON a.record_id = b.job_number
-    WHERE a.type <> 'Demolition'
-    AND a.status <> 'Withdrawn'
-    AND a.units_gross::int <> 0
-    AND b.units_prop::int > 0
-    AND NOT (a.type = 'Alteration'
-        and a.units_gross::integer <= 0)
-),
+/*
+DESCRIPTION:
+    Identies dcp_housing records that spatially overlap with 
+    and are within a few years of non-DOB records.
+INPUTS: 
+    _combined
+    dcp_housing_poly
+OUTPUTS: 
+    dob_review
+*/
+DROP TABLE IF EXISTS dob_review;
+WITH 
+-- Join project_record_ids to the combined source data
+projects AS (
+	SELECT
+		b.project_record_ids,
+		a.*
+	FROM _combined a
+	INNER JOIN _project_record_ids b
+ON a.record_id=any(b.project_record_ids)),
+/* 
+Identify records that intersect with DOB jobs. This excludes records from EDC 
+Projected Projects, and has a time constraint. Need to review how this
+constraint gets updated this year.
+*/
 matches as (
     SELECT 
-    b.source, 
-    b.record_id::text, 
-    b.record_name, 
-    b.status, 
-    b.type,
-    b.units_gross::integer, 
-    b.date, 
-    b.date_type,
-    b.date_filed,
-    b.date_complete,  
-    b.dcp_projectcompleted,
-    null as portion_built_by_2025, 
-    null as portion_built_by_2035, 
-    null as portion_built_by_2055,
-    a.cluster_id,
-    a.sub_cluster_id,
-    null as review_initials,
-    null as review_notes, 
-    a.project_id, 
-    null as units_net,
-    b.inactive,
-    b.geom
-    FROM combined a
-    INNER JOIN filtered_dcp_housing_proj b
+    	b.*,
+    	a.record_id as match_record_id,
+    	a.record_id_input as match_record_id_input,
+    	a.project_record_ids
+    FROM projects a
+    INNER JOIN _combined b
     ON st_intersects(a.geom, b.geom)
-    AND (case WHEN b.source = 'EDC Projected Projects' then TRUE 
-        else (CASE
-            WHEN b.date IS NOT NULL 
-                then extract(year from b.date::timestamp) >= 
+    AND (CASE 
+    		WHEN b.source = 'EDC Projected Projects' THEN TRUE 
+        	ELSE (CASE
+            	WHEN b.date IS NOT NULL 
+                	THEN extract(year from b.date::timestamp) >= 
                     split_part(split_part(a.date, '/', 1), '-', 1)::numeric - 2
-            ELSE extract(year from b.date::timestamp) >= 2020 -2
-            end)
-        end)),
+            	ELSE extract(year from b.date::timestamp) >= 2020 -2
+            END)
+        END)
+    WHERE b.source = 'DOB'
+    AND a.geom IS NOT NULL 
+    AND b.geom IS NOT NULL
+),
+-- Find cases where a DOB job matched with more than one project
+multimatch AS (
+    SELECT DISTINCT record_id
+    FROM matches
+    GROUP BY record_id
+    HAVING count(DISTINCT(project_record_ids)) > 1
+),
+/* 
+Find all projects where a DOB job matched with it, and that
+DOB job matched with more than one project.
+*/
+multimatchproject as (
+    SELECT project_record_ids
+    FROM matches
+    WHERE record_id IN (SELECT record_id FROM multimatch)
+),
+-- Combine matched DOB records with records from project table 
 combined_dob as (
-	select * 
-	from matches
-    union
-    select *
-	from combined),
-relevantcluster as (
-	select distinct project_id
-	FROM matches),
-multimatch as (
-    select distinct record_id
-    from matches
-    where source = 'DOB'
-    group by record_id
-    having count(distinct(project_id)) > 1),
-multimatchcluster as (
-    select distinct project_id
-    from combined_dob
-    where record_id in (select record_id from multimatch))
-select *,
-    (case when record_id in 
-	 (select record_id from multimatch) and source='DOB' then 1 
-        else 0 end) as dob_multimatch,
-    (case when project_id in 
-        (select project_id from multimatchcluster) then 1 else 0 end) as needs_review
-	into dob_review
-    from combined_dob
-	where project_id in (
-		select project_id 
-		from relevantcluster)
-	order by project_id;
+	SELECT
+		source,
+		record_id,
+		record_name,
+		status,
+		type,
+		units_gross,
+		date,
+		date_type,
+		inactive,
+		project_record_ids,
+		geom
+	FROM matches
+    UNION
+    SELECT 
+    	source,
+		record_id,
+		record_name,
+		status,
+		type,
+		units_gross,
+		date,
+		date_type,
+		inactive,
+		project_record_ids,
+		geom
+	FROM projects)
+-- Assign flags for review and append contextual DOB date and unit information
+SELECT a.*,
+		b.classa_init,
+		b.classa_prop,
+		b.otherb_init,
+		b.otherb_prop,
+		b.date_filed,
+		b.date_lastupdt,
+		b.date_complete,
+	    (CASE 
+	    	WHEN a.record_id IN (SELECT record_id FROM multimatch) AND a.source='DOB' THEN 1 
+	        ELSE 0
+	     END) as dob_multimatch,
+	    (CASE 
+	    	WHEN a.project_record_ids IN (SELECT project_record_ids FROM multimatchproject) THEN 1 
+	    	ELSE 0 
+	    END) as project_has_dob_multi
+INTO dob_review
+FROM combined_dob a
+LEFT JOIN dcp_housing_poly b
+ON a.record_id = b.record_id
+-- Only output matched DOB jobs and the records associated with them for review
+WHERE a.record_id IN (SELECT record_id FROM matches) 
+OR a.record_id IN (SELECT UNNEST(project_record_ids) FROM matches)
+ORDER BY project_record_ids;
--- knownprojects_build/sql/phasing_counts.sql
+++
@@ -1,4 +0,0 @@
-UPDATE kpdb."2020"
-SET within_5_years = ROUND(COALESCE(prop_within_5_years::decimal,0) * units_net::decimal),
-    from_5_to_10_years = ROUND(COALESCE(prop_5_to_10_years::decimal,0) * units_net::decimal),
-    after_10_years = ROUND(COALESCE(prop_after_10_years::decimal,0) * units_net::decimal);
