1515e1ae28e8798b278900814fd19452d031f18e
SPTKL <caobaiyue@gmail.com>
2021-03-16 10:34:21 -0400

    242 patch (#254)
    * applying corrections (#247)
    
    * applying corrections
    
    * only correct valid fields
    
    * fast forward submodule to get the latest corrections_main
    
    * move extractors to db-knownprojects-data
    
    * patch
    
    * Update dedup_units.py

--- knownprojects_build/02_build.sh
+++ knownprojects_build/02_build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 source config.sh
 
-# Create functions -- these get used to create flags
+# Create functions and procedures
 psql $BUILD_ENGINE -1 -f _sql/_functions.sql
+psql $BUILD_ENGINE -1 -f _sql/_procedures.sql
 
 # Map source data
 psql $BUILD_ENGINE -1 -f _sql/dcp_application.sql
 psql $BUILD_ENGINE -1 -f _sql/dcp_housing.sql
 psql $BUILD_ENGINE -1 -f _sql/combine.sql
+psql $BUILD_ENGINE -1 -c "CALL apply_correction('_combined');"
 
 # Find and matches between non-DOB sources
 psql $BUILD_ENGINE -1 -f _sql/_project_record_ids.sql
@@ -19,11 +21,10 @@ psql $BUILD_ENGINE -1 -f _sql/correct_projects.sql
 psql $BUILD_ENGINE -1 -f _sql/dob_match.sql
 
 # Create project IDs and deduplicate units
-psql $BUILD_ENGINE -1 -f _sql/project_record_ids.sql |
-docker run --rm\
-    -v $(pwd):/home/knownprojects_build\
-    -w /home/knownprojects_build\
-    python:3.7-slim sh -c "
-        pip3 install -q --disable-pip-version-check pandas; 
-        python3 _python/dedup_units.py" |
+psql $BUILD_ENGINE -1 -f _sql/project_record_ids.sql 
+
+# Dedup units
+python3 -m _python.dedup_units
+
+# Create KPDB
 psql $BUILD_ENGINE -1 -f _sql/create_kpdb.sql
