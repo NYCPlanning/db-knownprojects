7f981a9a9c0996af01bc3301b13e4e604a7bb360
Molly Graber <molly.graber@me.com>
2021-03-16 16:34:45 -0400

    WIP: Export (#266)
    * Add initial export scripts
    
    * Add project_id from deduped_units
    
    * Add project_id to kpdb
    
    Former-commit-id: 0321904097cab13d8d73af5707fe0a96c1e2bc7d

---
+++ knownprojects_build/sql/_export.sql
@@ -0,0 +1,38 @@
+/*
+DESCRIPTION:
+    Map KPDB data into final schema for export.
+
+INPUTS: 
+    _kpdb
+
+OUTPUTS: 
+    kpdb
+*/
+DROP TABLE IF EXISTS kpdb;
+SELECT
+    project_id,
+    source,
+    record_id,
+    record_name,
+    borough,
+    status,
+    type,
+    date,
+    date_type,
+    units_gross,
+    units_net,
+    prop_within_5_years,
+    prop_5_to_10_years,
+    prop_after_10_years,
+    within_5_years,
+    from_5_to_10_years,
+    after_10_years,
+    phasing_rationale,
+    phasing_known,
+    classb,
+    nycha,
+    senior_housing,
+    inactive,
+    geom
+INTO kpdb
+FROM _kpdb;
--- knownprojects_build/sql/create_kpdb.sql
+++ knownprojects_build/sql/create_kpdb.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS _kpdb;
 SELECT
     a.*,
+    b.project_id,
     get_boro(a.geom) as borough,
     b.units_net,
     ROUND(COALESCE(a.prop_within_5_years::decimal,0) * b.units_net::decimal) as within_5_years,
