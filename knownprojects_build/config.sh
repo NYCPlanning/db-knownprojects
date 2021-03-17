6eb53e7bff23da72554d51ff2b8d3cb5bdb866e5
SPTKL <caobaiyue@gmail.com>
2021-03-17 10:05:44 -0400

    Upload to submodule (#270)
    * New run uploaded Tue 16 Mar 2021 09:11:33 PM UTC
    
    * adding upload to github submodule step
    
    * fix submodule
    
    Former-commit-id: 2aa022610e14e6d32c2b21d9f99056e08ef57ec6

--- knownprojects_build/config.sh
+++ knownprojects_build/config.sh
@@ -4,6 +4,18 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
+function urlparse {
+    proto="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    url=$(echo $1 | sed -e s,$proto,,g)
+    userpass="$(echo $url | grep @ | cut -d@ -f1)"
+    BUILD_PWD=`echo $userpass | grep : | cut -d: -f2`
+    BUILD_USER=`echo $userpass | grep : | cut -d: -f1`
+    hostport=$(echo $url | sed -e s,$userpass@,,g | cut -d/ -f1)
+    BUILD_HOST="$(echo $hostport | sed -e 's,:.*,,g')"
+    BUILD_PORT="$(echo $hostport | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
+    BUILD_DB="$(echo $url | grep / | cut -d/ -f2-)"
+}
+
 function max_bg_procs {
     if [[ $# -eq 0 ]] ; then
             echo "Usage: max_bg_procs NUM_PROCS.  Will wait until the number of background (&)"
@@ -47,22 +59,19 @@ function CSV_export {
 }
 
 function SHP_export {
-  urlparse $1
-  mkdir -p $4 &&
-    (
-      cd $4
-      ogr2ogr -progress -f "ESRI Shapefile" $4.shp \
-          PG:"host=$BUILD_HOST user=$BUILD_USER port=$BUILD_PORT dbname=$BUILD_DB password=$BUILD_PWD" \
-          -nlt $3 $2
-        rm -f $4.zip
-        zip -9 $4.zip *
-        ls | grep -v $4.zip | xargs rm
-      )
-  mv $4/$4.zip $4.zip
-  rm -rf $4
-}
-
-function Upload {
-  mc rm -r --force spaces/edm-publishing/db-knownprojects/$@/
-  mc cp -r output spaces/edm-publishing/db-knownprojects/$@
+  urlparse $BUILD_ENGINE
+  table=$1
+  geomtype=$2
+  name=${3:-$table}
+  mkdir -p $name &&(
+    cd $name
+    ogr2ogr -progress -f "ESRI Shapefile" $name.shp \
+        PG:"host=$BUILD_HOST user=$BUILD_USER port=$BUILD_PORT dbname=$BUILD_DB password=$BUILD_PWD" \
+        $table -nlt $geomtype
+      rm -f $name.zip
+      zip -9 $name.zip *
+      ls | grep -v $name.zip | xargs rm
+  )
+  mv $name/$name.zip $name.zip
+  rm -rf $name
 }
