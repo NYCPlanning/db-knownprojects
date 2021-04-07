9b1d86e8ac54eff34f79754d39b03ec2d56bd7cf
mgraber <mgraber@planning.nyc.gov>
2021-04-07 15:44:07 +0000

    Switch to record-level oriented envelope in sq.mi

--- knownprojects_build/sql/review_project.sql
+++ knownprojects_build/sql/review_project.sql
@@ -1,51 +1,36 @@
 DROP TABLE IF EXISTS review_project;
-WITH 
-_review_project AS (
-    SELECT
-        a.source,
-        a.record_id,
-        a.record_name,
-        a.status,
-        a.type,
-        a.units_gross,
-        a.date,
-        a.date_type,
-        a.prop_within_5_years,
-        a.prop_5_to_10_years,
-        a.prop_after_10_years,
-        a.phasing_known,
-        a.nycha,
-        a.classb,
-        a.senior_housing,
-        array_to_string(b.project_record_ids, ',') as project_record_ids,
-        cardinality(b.project_record_ids) as records_in_project,
-        (cardinality(b.project_record_ids) > 1)::integer as multirecord_project,
-        b.dummy_id,
-        (a.geom IS NULL)::integer as no_geom,
-        a.geom,
-        NOW() as v
-    FROM combined a
-    LEFT JOIN (
-            SELECT 
-                project_record_ids,
-                unnest(project_record_ids) as record_id, 
-                ROW_NUMBER() OVER(ORDER BY project_record_ids) as dummy_id
-            FROM _project_record_ids
-        ) b 
-    ON a.record_id = b.record_id
-    WHERE source NOT IN ('DOB', 'Neighborhood Study Rezoning Commitments', 'Future Neighborhood Studies')
-),
-project_extent AS (
-    SELECT
-        dummy_id,
-        ST_Area(ST_SetSRID(ST_Extent(geom), 4326)::geography) as bbox_area
-    FROM _review_project
-    GROUP BY dummy_id
-)
 SELECT
-    a.*,
-    b.bbox_area
+    a.source,
+    a.record_id,
+    a.record_name,
+    a.status,
+    a.type,
+    a.units_gross,
+    a.date,
+    a.date_type,
+    a.prop_within_5_years,
+    a.prop_5_to_10_years,
+    a.prop_after_10_years,
+    a.phasing_known,
+    a.nycha,
+    a.classb,
+    a.senior_housing,
+    array_to_string(b.project_record_ids, ',') as project_record_ids,
+    cardinality(b.project_record_ids) as records_in_project,
+    (cardinality(b.project_record_ids) > 1)::integer as multirecord_project,
+    b.dummy_id,
+    ROUND((ST_Area(ST_OrientedEnvelope(a.geom)::geography))::numeric/(1609.34^2), 5) as bbox_area,
+    (a.geom IS NULL)::integer as no_geom,
+    a.geom,
+    NOW() as v
 INTO review_project
-FROM _review_project a
-JOIN project_extent b
-ON a.dummy_id = b.dummy_id;
+FROM combined a
+LEFT JOIN (
+        SELECT 
+            project_record_ids,
+            unnest(project_record_ids) as record_id, 
+            ROW_NUMBER() OVER(ORDER BY project_record_ids) as dummy_id
+        FROM _project_record_ids
+    ) b 
+ON a.record_id = b.record_id
+WHERE source NOT IN ('DOB', 'Neighborhood Study Rezoning Commitments', 'Future Neighborhood Studies');
