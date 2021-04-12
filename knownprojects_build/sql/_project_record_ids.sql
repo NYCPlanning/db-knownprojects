be79312de7e734147159291f167798c4802b5d6c
mgraber <mgraber@planning.nyc.gov>
2021-04-12 14:50:36 +0000

    Check for intersections prior to creating arrays
    Former-commit-id: 40e10a1a96bc7bee34522daf1f57c36b1eec4da8

--- knownprojects_build/sql/_project_record_ids.sql
+++ knownprojects_build/sql/_project_record_ids.sql
@@ -9,21 +9,44 @@ DESCRIPTION:
 
 INPUTS: 
 	combined
-    corrections_project (POST-REVIEW)
 OUTPUTS: 
     _project_record_ids
 */
 
 -- Identify spatially overlapping non-DOB records
 DROP TABLE IF EXISTS _project_record_ids;
-SELECT
-    array_agg(record_id) as project_record_ids
-INTO _project_record_ids
-FROM(
-    SELECT record_id, 
-    ST_ClusterDBSCAN(geom, 0, 1) OVER() AS id
+WITH
+dbscan AS (
+    SELECT 
+        record_id, 
+        geom,
+        ST_ClusterDBSCAN(geom, 0, 1) OVER() AS id
     FROM  combined
     WHERE source NOT IN ('DOB', 'Neighborhood Study Rezoning Commitments', 'Future Neighborhood Studies')
-) a
-WHERE id IS NOT NULL
-GROUP BY id;
+),
+project_record_join AS (
+	SELECT 
+	    a.record_id,
+	    COUNT(record_id) OVER(PARTITION BY id) as records_in_project,
+	    a.id,
+	    (a.geom IS NULL)::integer as no_geom,
+	    a.geom
+	FROM dbscan a
+),
+all_intersections AS (
+	SELECT 
+		ST_UNION(ST_INTERSECTION(a.geom, b.geom)) as intersect_geom
+	FROM  project_record_join a, project_record_join b
+	WHERE a.record_id < b.record_id
+	AND a.records_in_project > 1
+	AND b.records_in_project > 1
+	AND a.no_geom = 0
+	AND b.no_geom = 0
+)
+SELECT
+	array_agg(a.record_id) as project_record_ids
+--INTO _project_record_ids
+FROM project_record_join a, all_intersections b
+WHERE ST_OVERLAPS(a.geom, b.intersect_geom)
+AND a.id IS NOT NULL
+GROUP BY a.id;
