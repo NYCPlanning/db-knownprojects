61e96f0ec11ac58a213e318fe6742d358259152e
mgraber <mgraber@planning.nyc.gov>
2021-06-11 09:13:22 -0400

    Add outputs to debug

--- knownprojects_build/03_export.sh
+++ knownprojects_build/03_export.sh
@@ -33,6 +33,9 @@ mkdir -p output
         CSV_export corrections_dob_match &
         CSV_export corrections_project &
         CSV_export corrections_main &
+        CSV_export _project_record_ids &
+        CSV_export project_record_ids &
+        CSV_export deduped_units
         wait
         
         Compress combined.csv
