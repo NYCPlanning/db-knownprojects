1f7017eb56f3d6da93fe1dfb53acec4285ab0846
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-03-29 15:03:30 -0400

    update kpdb for borough rather than have logic in initial creation

--- sql/create_kpdb.sql
+++ sql/create_kpdb.sql
@@ -1,32 +1,6 @@
 DROP TABLE IF EXISTS _kpdb;
 SELECT
-    a.source,
-    a.record_id,
-    a.record_id_input,
-    a.record_name,
-    a.status,
-    a.type,
-    a.units_gross,
-    a.date,
-    a.date_type,
-    a.prop_within_5_years,
-    a.prop_5_to_10_years,
-    a.prop_after_10_years,
-    a.phasing_known,
-    a.geom,
-    a.nycha,
-    a.classb,
-    a.senior_housing,
-    a.inactive,
-    a.no_classa,
-    a.phasing_rationale,
-    b.project_id,
-    CASE 
-        WHEN a.borough IS NOT NULL THEN a.borough
-        WHEN a.source = 'DCP Application' AND a.record_id like 'P%' THEN ('{"M": 1, "X": 2, "K": 3, "Q": 4, "R": 5}'::json -> substring(a.record_id, 6, 1)) :: varchar
-        WHEN a.source = 'DCP Application' THEN ('{"M": 1, "X": 2, "K": 3, "Q": 4, "R": 5}'::json -> substring(a.record_id, 5, 1)) :: varchar
-        WHEN a.source = 'DOB' THEN substring(a.record_id, 1, 1)
-    END as borough,
+    a.*,
     COALESCE(b.units_net,a.units_gross) as units_net,
     ROUND(COALESCE(a.prop_within_5_years::decimal,0) * COALESCE(b.units_net,a.units_gross)::decimal) as within_5_years,
     ROUND(COALESCE(a.prop_5_to_10_years::decimal,0) * b.units_net::decimal) as from_5_to_10_years,
@@ -36,3 +10,13 @@ FROM combined a
 LEFT JOIN deduped_units b
 ON a.record_id = b.record_id
 WHERE a.no_classa = '0' OR a.no_classa IS NULL;
+
+UPDATE _kpdb 
+    SET borough = 
+        CASE 
+            WHEN a.borough IS NOT NULL THEN a.borough
+            WHEN a.source = 'DCP Application' AND a.record_id like 'P%' THEN ('{"M": 1, "X": 2, "K": 3, "Q": 4, "R": 5}'::json -> substring(a.record_id, 6, 1)) :: varchar
+            WHEN a.source = 'DCP Application' THEN ('{"M": 1, "X": 2, "K": 3, "Q": 4, "R": 5}'::json -> substring(a.record_id, 5, 1)) :: varchar
+            WHEN a.source = 'DOB' THEN substring(a.record_id, 1, 1)
+        END 
+WHERE borough IS NULL;
